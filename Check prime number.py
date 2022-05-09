f59e3077df7491bf17217d695159c4ca15ad92d5
Ashwit Achintam Sahu <70588743+ashwitsahu15@users.noreply.github.com>
2022-05-09 04:52:14 +0000

    Update Check prime number.py

--- Check prime number.py
+++ Check prime number.py
@@ -5,4 +5,16 @@ def prime(n) :
         if n%i==0 :
             return False
     return True
-
+# shortcut :
+import math
+def isprime(n) :
+    if n==1 :
+        return False
+    if n==2 or n==3 :
+        return True
+    if n%2==0 or n%3==0 :
+        return False
+    for i in range(5,int(math.sqrt(n))+1,5) :
+        if n%i==0 or n%(i+2)==0 :
+            return False
+    return True
